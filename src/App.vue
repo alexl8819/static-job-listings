44429177c73036bf55127b9eb2088213cec9f42b
alexl8819 <124635195+alexl8819@users.noreply.github.com>
2023-02-23 17:02:33 -0800

    cleanup: styling changes

--- src/App.vue
+++ src/App.vue
@@ -9,8 +9,8 @@
   const selectedFilter = ref(new Set());
   // Computed
   const availableJobs = computed(() => jobs.map((job) => Object.assign({}, job, {
-    logo: job.logo.replace(/.\/images\//g, ''),
-    selectable: job.tools.concat(job.languages, job.level, job.role).reverse()
+    logo: job.logo.replace(/.\/images\//g, ''), // we don't want the relative path used
+    selectable: [job.role, job.level].concat(job.languages, job.tools.reverse()) // job.tools.reverse().concat(job.role, job.level, job.languages)
   })));
   // Events
   const addToFilter = (filter) => selectedFilter.value.add(filter);
@@ -19,13 +19,13 @@
 </script>
 
 <template>
-  <header class="flex flex-col items-center h-32 pt-20 lg:pt-24 mb-24 lg:mb-10 bg-desaturated-dark-cyan bg-mobile-header lg:bg-desktop-header font-league-spartan">
-    <div class="max-w-xs lg:max-w-5xl">
+  <header class="flex flex-col items-center h-32 pt-20 lg:pt-24 mb-20 lg:mb-12 bg-desaturated-dark-cyan bg-mobile-header lg:bg-desktop-header font-league-spartan">
+    <div class="max-w-xs lg:max-w-7xl">
       <FilterBoard :selectedFilter="selectedFilter" @remove-filter="removeFromFilter" @remove-all="removeAll" />
     </div>
   </header>
   <div class="flex flex-col justify-start items-center min-h-screen font-league-spartan">
-    <div class="max-w-xs lg:max-w-5xl">
+    <div class="max-w-xs lg:max-w-7xl">
       <JobsList :jobs="availableJobs" :selectedFilter="selectedFilter" @add-filter="addToFilter" />
     </div>
   </div>
--- src/components/JobsList.vue
+++ src/components/JobsList.vue
@@ -8,36 +8,38 @@ const props = defineProps({
 
 // Computed
 const filteredJobs = computed(() => !props.selectedFilter.size ? props.jobs : props.jobs.filter((job) => Array.from(props.selectedFilter).every((v) => job.selectable.includes(v))));
-// Method
-const isFeaturedNew = (job) => job.new || job.featured ? 'border-l-4 border-l-desaturated-dark-cyan' : '';
+// Methods
+const isFeaturedNew = (job) => job.new && job.featured ? 'border-l-4 border-l-desaturated-dark-cyan' : '';
 const renderLogo = (imgName) => new URL(`../assets/images/${imgName}`, import.meta.url).href;
 </script>
 
 <template>
-  <div class="space-y-6 py-2">
+  <div class="space-y-6 my-2">
     <template v-for="job in filteredJobs" :key="job.id">
-      <article class="flex flex-col lg:flex-row justify-between lg:items-center py-6 lg:py-4 px-6 rounded-md shadow-lg bg-white" :class="isFeaturedNew(job)">
-        <div class="flex flex-col lg:flex-row lg:items-center">
+      <article class="flex flex-col lg:flex-row justify-between lg:items-center my-4 py-4 lg:py-2 px-6 rounded-md shadow-lg bg-white" :class="isFeaturedNew(job)">
+        <div class="flex flex-col lg:flex-row lg:items-center lg:w-[37rem]">
           <div class="lg:w-20">
-            <img :src='renderLogo(job.logo)' class="h-10 lg:h-16 w-10 lg:w-16 -mt-10 lg:-mt-0 lg:mr-2" alt="company logo" />
+            <img :src='renderLogo(job.logo)' class="h-10 lg:h-16 w-10 lg:w-16 -mt-10 lg:-mt-0" alt="company logo" />
           </div>
-          <div class="lg:w-64 border-b lg:border-none border-b-dark-greyish-cyan mb-2">
-            <div class="flex flex-row items-center my-3">
+          <div class="border-b lg:border-none border-b-dark-greyish-cyan mb-2 lg:mb-0">
+            <div class="flex flex-row items-center my-3 lg:my-1">
               <h1 class="text-sm text-desaturated-dark-cyan">{{ job.company }}</h1>
-              <div class="flex flex-row ml-6 space-x-3">
+              <div class="flex flex-row ml-6 lg:ml-4 space-x-2.5">
                 <span class="px-2 py-1 uppercase rounded-full bg-desaturated-dark-cyan text-xs text-white" v-if="job.new">new!</span>
                 <span class="px-2 py-1 uppercase rounded-full bg-black text-xs text-white" v-if="job.featured">featured</span>
               </div>
             </div>
             <p class="font-bold cursor-pointer text-very-dark-greyish-cyan hover:text-desaturated-dark-cyan">{{ job.position }}</p>
-            <ul class="list-disc my-3 space-x-4 flex flex-row items-center">
-              <li class="first:list-none text-xs text-dark-greyish-cyan">{{ job.postedAt }}</li>
+            <ul class="list-none mt-1 mb-4 space-x-3.5 flex flex-row items-center"> <!--list-dict -->
+              <li class="text-xs text-dark-greyish-cyan">{{ job.postedAt }}</li> <!-- first:list-none -->
+              <li class="text-xs text-dark-greyish-cyan">•</li>
               <li class="text-xs text-dark-greyish-cyan">{{ job.contract }}</li>
+              <li class="text-xs text-dark-greyish-cyan">•</li>
               <li class="text-xs text-dark-greyish-cyan">{{ job.location }}</li>
             </ul>
           </div>
         </div>
-        <div class="mt-6 lg:mt-0">
+        <div class="mt-6 lg:mt-0 lg:h-12 lg:py-2">
           <button class="inline-block mr-2 mb-6 py-2 px-4 rounded-lg bg-light-greyish-cyan-2 hover:bg-desaturated-dark-cyan text-desaturated-dark-cyan hover:text-white lg:text-xs cursor-pointer" @click="$emit('addFilter', tablet)" v-for="tablet in job.selectable">
             {{ tablet }}
           </button>

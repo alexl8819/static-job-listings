e23ac7cbc5298966a348e14449c0ad7cdcce0a5c
alexl8819 <124635195+alexl8819@users.noreply.github.com>
2023-08-23 16:35:11 -0700

    fix: removes default import used for svg loader

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import svgLoader from 'vite-svg-loader';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), svgLoader({ defaultImport: 'url' })],
+  plugins: [vue(), svgLoader()],
   publicDir: false,
   build: {
     outDir: 'public'
